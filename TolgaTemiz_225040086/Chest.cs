406a205860ce2e6ad5feee5191469392f55cabc4
TolgaTemizz <tolga.temiz@outlook.com>
2025-01-27 17:40:45 +0300

    Chest.cs

--- TolgaTemiz_225040086/Chest.cs
+++ TolgaTemiz_225040086/Chest.cs
@@ -1,4 +1,3 @@
-﻿// Chest.cs
 using System;
 
 class Chest : IInteractable
@@ -14,5 +13,35 @@ class Chest : IInteractable
     {
         Console.WriteLine("You open the chest and find something inside!");
         inventory.AddItem(item);
+
+        // Eşyayı kullanmak için seçenek
+        Console.WriteLine("Do you want to use this item?");
+        Console.WriteLine("1. Yes");
+        Console.WriteLine("2. No");
+
+        string choice = Console.ReadLine();
+
+        if (choice == "1")
+        {
+            UseItem(inventory);
+        }
+        else
+        {
+            Console.WriteLine("You decide not to use the item.");
+        }
+        Console.WriteLine("Press any key to continue.");
+        Console.ReadKey();
+    }
+
+    private void UseItem(Inventory inventory)
+    {
+        if (item == "Ancient Coin")
+        {
+            Console.WriteLine("You use the Ancient Coin and hear a distant sound of a door opening...");
+        }
+        else
+        {
+            Console.WriteLine("This item has no immediate effect.");
+        }
     }
 }
