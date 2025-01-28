f039adac22c0b941e12014b053712d18d884fa2b
TolgaTemizz <tolga.temiz@outlook.com>
2025-01-28 14:07:38 +0300

    Add files via upload

---
+++ TolgaTemiz_225040086/SaveData.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+class SaveData
+{
+    public string CurrentLocation { get; set; }
+    public List<string> InventoryItems { get; set; }
+}
